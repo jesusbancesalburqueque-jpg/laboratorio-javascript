1b5586c8994f7f7aa697e555101858a792776a29
jesusbancesalburqueque-jpg <jesusbancesalburqueque@gmail.com>
2026-06-18 20:33:14 -0500

    Fundamentos y literales completados

---
+++ mian.js
@@ -0,0 +1,21 @@
+console.log("Iniciando laboratorio 1 de la unidad 3");
+
+let edad = 20;
+let esEstudiante = true;
+
+console.log(typeof edad);
+console.log(typeof esEstudiante);
+
+let nombre = "Jesús";
+
+let saludo = `Hola $(nombre), tienes $(edad) años`;
+
+console.log(saludo);
+
+let mensaje = `
+Este es mi laboratorio:
+Semana 12,
+Unidad 3
+`;
+
+console.log(mensaje);
